427a5fd8c5205cfe48e1541d8415e4dd6fd21dcc
Deniz Akşimşek <d.aksimsek1@outlook.com>
2019-06-22 16:17:35 +0300

    add color selection

--- src/pages/note-page.js
+++ src/pages/note-page.js
@@ -1,6 +1,6 @@
 import {html, render} from 'https://unpkg.com/lit-html@1.0.0/lit-html.js'
 import {until} from 'https://unpkg.com/lit-html@1.0.0/directives/until.js'
-
+import {repeat} from 'https://unpkg.com/lit-html@1.0.0/directives/repeat.js'
 export default (noteRepo, id) => {
   const note$ = noteRepo.getNoteById(id)
   const render = note => {
@@ -20,12 +20,33 @@ export default (noteRepo, id) => {
       window.history.back()
     }
 
+    const changeColor = color => {
+      const el = document.querySelector('.note.page')
+      el.classList.remove(`note-color-${note.color}`)
+      note.color = color
+      noteRepo.editNote(note)
+      el.classList.add(`note-color-${color}`)
+      // shh
+    }
+    
+    const colors = ['yellow', 'red', 'blue', 'green', 'pink', 'black']
+
+    const colorDropdown = onselect => html`
+      <div class='color-dropdown'>
+        <a>color</a>
+        <div class="color-dropdown-content">
+          ${repeat(colors, color => html`
+            <a class="note-color-${color}"
+              @click="${() => onselect(color)}"></div>`)}
+        </div>
+      </div>`
+
     return html`
       <div class="note page note-color-${note.color}">
         <header>
             <a href=# @click="${saveChangesAndExit}">back</a>
             <a @click="${deleteNote}">delete</a>
-            <a>color</a>
+            ${colorDropdown(changeColor)}
         </header>
         <main>
           <textarea .value="${note.content}" 
